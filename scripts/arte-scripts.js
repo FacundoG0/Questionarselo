34cbdea8b3e6db082c66c343e64b62f9ea648afc
Facundo Gordillo <gordillofacundo44@gmail.com>
2022-07-14 07:28:17 -0300

    prueba script respuestas

---
+++ scripts/arte-scripts.js
@@ -0,0 +1,7 @@
+var prueba = document.getElementById("arte1");
+//document.getElementById("arte2").onclick.value = 2;
+//document.getElementById("arte3").onclick.value = 3;
+//document.getElementById("arte4").onclick.value = 4;
+
+function prueba1(){
+    prueba.onclick = 1;}
